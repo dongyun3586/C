f618835b0ba4c22dc35ac2f46b8b7ff87dfd5c05
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-03-10 13:20:55 +0900

    강의실 PC 자료 Commit

---
+++ Class1/HelloWorld/HelloWorld/Hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main_Hello(void) {
+	printf("Hello world~");
+	return 0;
+}
---
+++ Class1/HelloWorld/HelloWorld/Hi.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main_Hi(void) {
+	printf("Hi\nworld~ \n");
+	return 0;
+}
