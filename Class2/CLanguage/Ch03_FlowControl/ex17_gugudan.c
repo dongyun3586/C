54b7524f0930750e0954d9430f5a107c2a36ce4c
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-04-01 10:27:07 +0900

    4월 1일 2반 수업

---
+++ Class2/CLanguage/Ch03_FlowControl/ex17_gugudan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main_ex17_gugudan() {
+	int dan, n;
+	for (dan = 2; dan <= 9; dan++) {
+		for (n = 1; n <= 9; n++) 
+			printf("%2d * %2d = %2d\n", dan, n, dan * n);
+		
+		printf("\n");
+	}
+
+	return 0;
+}
