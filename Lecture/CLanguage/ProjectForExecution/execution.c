f49ab165b95b2ad03bc2c5a4faa12b4b820e70c3
dongyun3586 <goodluck3586@gmail.com>
2021-04-02 16:06:16 +0900

    gg

--- Lecture/CLanguage/ProjectForExecution/execution.c
+++ Lecture/CLanguage/ProjectForExecution/execution.c
@@ -1,20 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	time_t t;
-	t = time();      // time() 함수를 통해서 현재 시간 반환
-	printf("%lld\n", t);   // 출력
+    int i=1, number, sum = 0;
 
-	// time() : 1970년 1월 1일 00시 00분(UTC) 부터 지금까지 초단위의 시간에 대한 정수값 반환
-	printf("%d\n", time(NULL));  // 현재 시간 출력
+    while (1) {
+        printf("정수입력 %d: ", i);
+        scanf("%d", &number);
 
+        // 음수가 입력되면 종료된다.
+        if (number < 0)
+            break;
 
+        i++;
+        sum += number;
+    }
 
+    printf("합계 = %d", sum);
 
-
-	//for (int i = 0; i < 10; i++) {
-	//	
-	//}
-
-	return 0;
+    return 0;
 }
