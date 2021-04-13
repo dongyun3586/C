1cbe1f151212f5ac6426d3c842f7ceef16c001a9
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-04-13 10:42:51 +0900

    gg

--- Class5/CLanguage/Ch03_FlowControl/ex19_printInvertedHalfPyramid.c
+++ Class5/CLanguage/Ch03_FlowControl/ex19_printInvertedHalfPyramid.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main_ex19_printInvertedHalfPyramid() {
     int i, j, height;
     printf("피라미드의 높이: ");
     scanf("%d", &height);
---
+++ Class5/CLanguage/Ch03_FlowControl/ex25_randomGugudan.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+	// 변수 선언
+	int dan, i, jungdap, userdap, score = 0, n;
+
+	// 사용자 입력 받기
+	printf("몇 문제? ");
+	scanf("%d", &n);
+
+	// 처리
+	srand(time(NULL));
+	for (int a = 0; a < n; a++) {
+		dan = rand() % 8 + 2;	// 2~9
+		i = rand() % 9 + 1;		// 1~9
+		jungdap = dan * i;
+
+		printf("%d * %d = ", dan, i);
+		scanf("%d", &userdap);
+
+		if (userdap == jungdap) {
+			printf("정답입니다. \n");
+			score++;
+		}
+		else
+			prtinf("틀렸어요. 정답은 %d 입니다. \n", jungdap);
+	}
+
+	// 최종 결과 출력
+	printf("%d 문제 중 %d 개 맞음\n", n, score);
+
+	return 0;
+}
