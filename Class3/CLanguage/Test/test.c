3481fa970ef891adb72db48cee47a57d217e09ab
dongyun3586 <goodluck3586@gmail.com>
2021-04-15 08:19:10 +0900

    gg

--- Class3/CLanguage/Test/test.c
+++ Class3/CLanguage/Test/test.c
@@ -16,40 +16,40 @@ int main() {
 #pragma endregion
 
 #pragma region 2. rand(), srand() 사용
-    //// 함수원형: void srand (unsigned int seed) 
-    //// 함수기능: Initialize random number generator.
-    //// rand 함수는 내부적으로 어떤 srand의 매개변수로 들어온 seed 값에 해당하는 숫자들을 반환한다.
+    // 함수원형: void srand (unsigned int seed) 
+    // 함수기능: Initialize random number generator.
+    // rand 함수는 내부적으로 어떤 srand의 매개변수로 들어온 seed 값에 해당하는 숫자들을 반환한다.
 
-    //// srand(seed)에 전달되는 seed값이 같으면 rand()함수가 생성하는 정수값들은 같다.
-    //srand(100);
-    //for (int i = 0; i < 10; i++)
-    //    printf("rand : %d\n", rand());
-    //printf("\n");
+    // srand(seed)에 전달되는 seed값이 같으면 rand()함수가 생성하는 정수값들은 같다.
+    srand(100);
+    for (int i = 0; i < 10; i++)
+        printf("rand : %d\n", rand());
+    printf("\n");
 
-    //srand(100);
-    //for (int i = 0; i < 10; i++)
-    //    printf("rand : %d\n", rand());
-    //printf("\n");
+    srand(100);
+    for (int i = 0; i < 10; i++)
+        printf("rand : %d\n", rand());
+    printf("\n");
 
-    //// srand(seed)에 전달되는 seed값이 다르면 rand()함수가 생성하는 정수값들도 다르다.
-    //srand(200);
-    //for (int i = 0; i < 10; i++)
-    //    printf("rand : %d\n", rand());
+    // srand(seed)에 전달되는 seed값이 다르면 rand()함수가 생성하는 정수값들도 다르다.
+    srand(200);
+    for (int i = 0; i < 10; i++)
+        printf("rand : %d\n", rand());
 #pragma endregion
 
 #pragma region 3. rand(), srand(), time() 사용
-    // 함수원형: time_t time(time_t* timer)
-    // 함수기능: UCT 기준 1970년 1월 1일 0시 0분 0초 부터 경과된 시간을 초(sec)로 반환하는 함수
+    //// 함수원형: time_t time(time_t* timer)
+    //// 함수기능: UCT 기준 1970년 1월 1일 0시 0분 0초 부터 경과된 시간을 초(sec)로 반환하는 함수
 
-    // 프로그램을 실행할 때 마다 다른 랜덤 숫자를 얻기 위해서는 항상 변하는 seed값을 이용하면 된다.
-    printf("%d\n", time(NULL));  
-    printf("1970년 부터 몇 초나 지났나? : %ld 초 \n", time(NULL));
+    //// 프로그램을 실행할 때 마다 다른 랜덤 숫자를 얻기 위해서는 항상 변하는 seed값을 이용하면 된다.
+    //printf("%d\n", time(NULL));  
+    //printf("1970년 부터 몇 초나 지났나? : %ld 초 \n", time(NULL));
 
-    srand(time(NULL));
-    for (int i = 0; i < 100; i++)
-        printf("rand : %d\n", rand()%8+2);
+    //srand(time(NULL));
+    //for (int i = 0; i < 100; i++)
+    //    printf("rand : %d\n", rand()%8+2);
 
-    printf("\n");
+    //printf("\n");
 #pragma endregion
 
     return 0;
