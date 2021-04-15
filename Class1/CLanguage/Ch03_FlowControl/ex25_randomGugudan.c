9dc64d43f01bc5373a9e133de0ac2748cf87d1b7
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-04-15 09:05:49 +0900

    gg

--- Class1/CLanguage/Ch03_FlowControl/ex25_randomGugudan.c
+++ Class1/CLanguage/Ch03_FlowControl/ex25_randomGugudan.c
@@ -4,36 +4,32 @@
 
 int main() {
     // 1. 변수 선언
-    int dan, i, jungdap, userdap, n, score = 0;
+    int dan, i, jungdap, userdap, score = 0, n;
 
-    // 2. 사용자 입력
-    printf("몇 문제? ");
+    // 2. 사용자 입력 받기
+    printf("구구단 몇 문제? ");
     scanf("%d", &n);
 
     // 3. 처리
     srand(time(NULL));
     for (int a = 0; a < n; a++) {
-        // 랜덤 구구단 문제 생성
-        dan = rand() % 8 + 2;   // 2~9 
+        dan = rand() % 8 + 2;   // 2~9
         i = rand() % 9 + 1;     // 1~9
         jungdap = dan * i;
 
-        // 사용자 답 입력받기
         printf("%d * %d = ", dan, i);
         scanf("%d", &userdap);
 
-        // 맞았는지 판단
         if (userdap == jungdap) {
-            printf("맞았습니다. \n");
+            printf("정답입니다. \n");
             score++;
         }
-        else {
-            printf("틀렸습니다.정답은 %d 입니다. \n", jungdap);
-        }
+        else
+            printf("틀렸습니다. 정답은 %d 입니다.\n", jungdap);
     }
 
     // 4. 결과 출력
-    printf("%d문제중 %d 개 맞았습니다. \n", n, score);
+    printf("%d 개 중 %d개 맞았습니다. \n", n, score);
 
     return 0;
 }
