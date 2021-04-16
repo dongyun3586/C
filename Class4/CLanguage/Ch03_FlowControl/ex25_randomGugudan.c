0b0dcb6389b1fbad36dc5d8cd67f9adcfeedc96e
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-04-16 12:11:04 +0900

    gg

--- Class4/CLanguage/Ch03_FlowControl/ex25_randomGugudan.c
+++ Class4/CLanguage/Ch03_FlowControl/ex25_randomGugudan.c
@@ -3,11 +3,14 @@
 #include<time.h>
 
 int main() {
+	// 변수 선언
 	int dan, i, jungdap, userdap, n, count=0;
 
+	// 값 입력받기
 	printf("구구단 몇 개? ");
 	scanf("%d", &n);
 
+	// 처리
 	srand(time(NULL));
 	for (int a = 0; a < n; a++) {
 		// 문제 랜덤 생성
@@ -28,7 +31,8 @@ int main() {
 			printf("틀렸습니다. 정답은 %d 입니다. \n", jungdap);
 	}
 
-
+	// 결과 출력
+	printf("%d 문제 중 %d개 맞았습니다. \n", n, count);
 
 	return 0;
 }
