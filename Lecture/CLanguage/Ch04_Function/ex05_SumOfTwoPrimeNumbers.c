bcda33115e3b625e0b83c49592e281aa4e5412d9
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-05-10 10:43:56 +0900

    ㅎㅎ

--- Lecture/CLanguage/Ch04_Function/ex05_SumOfTwoPrimeNumbers.c
+++ Lecture/CLanguage/Ch04_Function/ex05_SumOfTwoPrimeNumbers.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 int main_ex05_SumOfTwoPrimeNumbers() {
-    int n, flag = 0;
+    int n, flag = 1;
     printf("양의 정수: ");
     scanf("%d", &n);
 
     // n을 소수의 합으로 표현되는 경우 출력
     for(int i = 2; i <= n / 2; ++i) {
         // condition for i to be a prime number
-        if (checkPrimeNumber(i) == 1) {
-            // condition for n-i to be a prime number
-            if (checkPrimeNumber(n - i) == 1) {
+        if (checkPrimeNumber(i) && checkPrimeNumber(n - i)) {
                 printf("%d = %d + %d\n", n, i, n - i);
-                flag = 1;
-            }
+                flag = 0;
         }
     }
 
-    if(flag == 0)
+    if(flag)
         printf("%d 는 소수의 합으로 표현할 수 없습니다.\n", n);
 
     return 0;
--- Lecture/CLanguage/Test/test.c
+++ Lecture/CLanguage/Test/test.c
@@ -1,71 +1,30 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
-int countDigitsOfNumber(int num);
-int checkArmstrongNumber(int num, int n);
-int reversNumber(int num);
-void printArmstrongNumber(int num, int n);
+int checkPrimeNumber(int n);
 
 int main() {
-	int a, b, n;
+    int n, flag = 1;
+    printf("양의 정수: ");
+    scanf("%d", &n);
 
-	printf("암스트롱 정수 범위(양의 정수 두 개): ");
-	scanf("%d %d", &a, &b);
+    // n을 소수의 합으로 표현되는 경우 출력
+    for (int i = 2; i <= n / 2; ++i) {
+        // condition for i to be a prime number
+        if (checkPrimeNumber(i) && checkPrimeNumber(n - i)) {
+            printf("%d = %d + %d\n", n, i, n - i);
+            flag = 0;
+        }
+    }
 
-	// a ~ b 사이의 암스트롱 넘버 출력하기
-	for (int i = a; i <= b; i++) {
-		n = countDigitsOfNumber(i);
-		if (checkArmstrongNumber(i, n)) {
-			printf("%7d = ", i);
-			printArmstrongNumber(reversNumber(i), n);
-		}
-	}
-	return 0;
+    if (flag)
+        printf("%d 는 소수의 합으로 표현할 수 없습니다.\n", n);
+
+    return 0;
 }
 
-// num의 자릿수 n 구하기
-int countDigitsOfNumber(int num) {
-	int n = 0;
-	while (num != 0) {
-		num /= 10;
-		n++;
-	}
-	return n;
+int checkPrimeNumber(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i < n; i++)
+        if (n % i == 0) return 0;
+    return 1;
 }
-
-// num이 암스트롱 넘버인지 체크(맞으면 1, 틀리면 0 반환)
-int checkArmstrongNumber(int num, int n) {
-	int sum = 0, originalNum, remainder;
-
-	// num의 자릿수를 n 제곱한 합 sum 구하기
-	for (originalNum = num; originalNum != 0; originalNum /= 10) {
-		remainder = originalNum % 10;
-		sum += pow(remainder % 10, n);
-	}
-
-	// 암스트롱 넘버 유무 리턴
-	return (sum == num) ? 1 : 0;
-}
-
-// num의 각 자리 숫자 뒤집기
-int reversNumber(int num) {
-	int rev = 0, remainder;
-
-	while (num != 0) {
-		remainder = num % 10;
-		rev = rev * 10 + remainder;
-		num /= 10;
-	}
-	return rev;
-}
-
-// abc = a*a*a + b*b*b + c*c*c의 형태로 출력하기
-void printArmstrongNumber(int num, int n) {
-	while (num != 0) {
-		printf("%d ", (int)pow(num % 10, n));
-		num /= 10;
-		if (num != 0) printf("+ ");
-	}
-	printf("\n");
-}
-
