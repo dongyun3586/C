0d9ae9b3005cfa3e22cdff4a508295ee7b2d96bc
DESKTOP-7GUMC2L\Administrator <goodluck3586@gmail.com>
2021-05-14 12:20:26 +0900

    gg

---
+++ Class4/CLanguage/Ch04_Function/ex06_SumOfTwoPrimeNumbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int checkPrimeNumber(int n);
+
+int main() {
+    int n, flag = 1;     // flag 값을 이용하여 소수의 합으로 표현할 수 없는 경우 처리
+    printf("양의 정수: ");
+    scanf("%d", &n);
+
+    // n을 소수의 합으로 표현되는 경우를 모두 출력
+
+    return 0;
+}
+
+int checkPrimeNumber(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i < n; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
--- Class4/CLanguage/Ch04_Function/ex08_array.c
+++ Class4/CLanguage/Ch04_Function/ex08_array.c
@@ -3,7 +3,7 @@
 int main() {
 	// 배열 선언 및 초기화 
 	int arr1[5];					// 초기화 없이 선언
-	int arr2[500] = { 0 };	// arr2[5] = { 0 }
+	int arr2[5] = { 0,0,0,0,0 };	// arr2[5] = { 0 }
 	int arr3[] = { 1,2,3,4,5 };		// 배열을 선언할 때 크기 생략 가능
 
 	// 배열의 특정 요소에 값 대입 => 배열이름[index]로 접근
@@ -14,7 +14,7 @@ int main() {
 	for (int i = 0; i < 5; i++)
 		printf("arr1[%d] = %d\n", i, arr1[i]);
 
-	for (int i = 0; i < 500; i++)
+	for (int i = 0; i < 5; i++)
 		printf("arr2[%d] = %d\n", i, arr2[i]);
 
 	for (int i = 0; i < 5; i++)
