45c647c2b9c9cdd7d43301fad723b1ceb086a6ac
dongyun3586 <goodluck3586@gmail.com>
2021-05-18 14:29:40 +0900

    gg

--- Lecture/CLanguage/Test/test.c
+++ Lecture/CLanguage/Test/test.c
@@ -1,32 +1,40 @@
-﻿#include<stdio.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int ans, recept, count = 0;
+	srand(time(NULL));
+	ans = rand() % 100 + 1;
+	printf("*** 1학년 6반 17번 한지민 ***\n1~100사이의 숫자를  추측하는 게임입니다.\n");
+	printf("\n");
+	printf("정답: %d\n", ans);
+	printf("\n");
 
-void print1DArray(int arr[], int length);
-void print2DArray(int arr[][4], int row, int column);
+	do
+	{
+		printf("정답은?: ");
+		scanf("%d", &recept);
+		if (recept != ans)
+		{
+			if (recept > ans)
+			{
+				printf("틀렸습니다. %d 보다 작습니다.\n", recept);
+			}
+			else
+			{
+				printf("틀렸습니다. %d 보다 큽니다.\n", recept);
+			}
+		}
+		else
+		{
+			printf("정답입니다.\n");
+		}
+		count++;
+	} while (recept != ans);
+	printf("\n");
+	printf("%d번 만에 맞혔습니다.\n", count);
 
-int main() {
-	int arr1[] = { 1,2,3,4,5,6,7,8,9 };
-	int arr2[5][4] = { 0 };
-	int row = sizeof(arr2) / sizeof(arr2[0]);
-	int column = sizeof(arr2[0]) / sizeof(int);
-
-	print1DArray(arr1, sizeof(arr1) / sizeof(int));
-	print2DArray(arr2, row, column);
 
 	return 0;
-}
-
-void print1DArray(int arr[], int length) {
-	printf("1차원 배열 출력\n");
-	for (int i = 0; i < length; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
-
-void print2DArray(int arr[][4], int row, int column) {
-	printf("\n2차원 배열 출력\n");
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++)
-			printf("%d ", arr[i][j]);
-		printf("\n");
-	}
 }
