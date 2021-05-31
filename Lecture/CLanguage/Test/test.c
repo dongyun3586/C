ada85c044b47f256b85ada18a82a78dfcac6212d
dongyun3586 <goodluck3586@gmail.com>
2021-05-31 21:44:11 +0900

    gg

--- Lecture/CLanguage/Test/test.c
+++ Lecture/CLanguage/Test/test.c
@@ -1,87 +1,32 @@
 ﻿#include <stdio.h>
 
-#define SIZE 3 // Size of the matrix
+/* Function declaration */
+int gcd(int a, int b);
 
-void print2DArray(int arr[][SIZE], int row, int column);
-void addMatrices(int A[][SIZE], int B[][SIZE], int C[][SIZE]);
-void subtractMatrices(int A[][SIZE], int B[][SIZE], int C[][SIZE]);
-void multiplyMatrices(int A[][SIZE], int B[][SIZE], int C[][SIZE]);
 
 int main()
 {
-    int A[SIZE][SIZE] = { 1,2,3,4,5,6,7,8,9 };
-    int B[SIZE][SIZE] = { 9,8,7,6,5,4,3,2,1 };
-    int C[SIZE][SIZE] = { 0 };
+    int num1, num2, hcf;
 
-    int row, col, i, sum;
+    /* Input two numbers from user */
+    printf("Enter any two numbers to find GCD: ");
+    scanf("%d%d", &num1, &num2);
 
-    // 행렬 A와 B 출력
-    printf("행렬 A\n");
-    print2DArray(A, SIZE, SIZE);
-    printf("행렬 B\n");
-    print2DArray(B, SIZE, SIZE);
+    hcf = gcd(num1, num2);
 
-    // 행렬의 덧셈
-    addMatrices(A, B, C);
-    printf("행렬 A, B의 덧셈 결과\n");
-    print2DArray(C, SIZE, SIZE);
-
-    // 행렬의 뺄셈
-    subtractMatrices(A, B, C);
-    printf("행렬 A, B의 뺄셈 결과\n");
-    print2DArray(C, SIZE, SIZE);
-
-    // 행렬의 곱셈
-    multiplyMatrices(A, B, C);
-    printf("행렬 A, B의 곱셈 결과\n");
-    print2DArray(C, SIZE, SIZE);
+    printf("GCD of %d and %d = %d", num1, num2, hcf);
 
     return 0;
 }
 
-void print2DArray(int arr[][SIZE], int row, int column) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++)
-            printf("%3d ", arr[i][j]);
-        printf("\n");
-    }
-    printf("\n");
-}
 
-void addMatrices(int A[][SIZE], int B[][SIZE], int C[][SIZE])
+/**
+ * Recursive approach of euclidean algorithm to find GCD of two numbers
+ */
+int gcd(int a, int b)
 {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++)
-            C[row][col] = A[row][col] + B[row][col];
-    }
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
 }
-
-void subtractMatrices(int A[][SIZE], int B[][SIZE], int C[][SIZE])
-{
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++)
-            C[row][col] = A[row][col] - B[row][col];
-    }
-}
-
-void multiplyMatrices(int A[][SIZE], int B[][SIZE], int C[][SIZE])
-{
-    int sum;
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
-            sum = 0;    // Multiply row of first matrix to column of second matrix and store sum of product of elements in sum.
-            for (int i = 0; i < SIZE; i++)
-                sum += A[row][i] * B[i][col];
-            C[row][col] = sum;
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
