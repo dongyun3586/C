52227487928556feaa1e6b1855a557422adae7bf
dongyun3586 <goodluck3586@gmail.com>
2021-08-30 21:40:36 +0900

    포인터 21번 예제까지 준비

---
+++ Lecture/CLanguage/Ch05_Pointer/ex20_functionPointer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int calculator(int a, int b, int(*calcFunc)(int, int)) {
+    return calcFunc(a, b);
+}
+
+int main()
+{
+    int a = 3, b = 4;
+
+    printf("Че: %d\n", calculator(a, b, sum));
+    printf("Тї: %d\n", calculator(a, b, subtract));
+
+    return 0;
+}
--- Lecture/CLanguage/CodeTest/test.c
+++ Lecture/CLanguage/CodeTest/test.c
@@ -1,57 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-void printCardInfo(char* cardOwner, void** card);
-void swapCard(void** c1, void** c2);
-void cardBattle(void** myCard, void** comCard);
-
-int main() {
-	char nameA[10] = "유비", nameB[10] = "조조";
-	int infoA[5] = { 75, 73, 74, 78, 99 }, infoB[5] = { 96, 72, 91, 94, 96 };
-
-	void* cardA[2] = { nameA, infoA };
-	void* cardB[2] = { nameB, infoB };
-	void* myCard = cardA;
-	void* comCard = cardB;
-
-	printCardInfo("플레이어 카드", myCard);
-	printCardInfo("컴퓨터 카드", comCard);
-
-	cardBattle(myCard, comCard);
-
-	printf("\n****** Card trading ******\n\n");
-	swapCard(&myCard, &comCard);
-
-	printCardInfo("플레이어 카드", myCard);
-	printCardInfo("컴퓨터 카드", comCard);
-
-	cardBattle(myCard, comCard);
-
-	return 0;
+void changePosition(char* ch1, char* ch2)
+{
+    char tmp;
+    tmp = *ch1;
+    *ch1 = *ch2;
+    *ch2 = tmp;
+}
+void charPermu(char* cht, int stno, int endno)
+{
+    int i;
+    if (stno == endno)
+        printf("%s  ", cht);
+    else
+    {
+        for (i = stno; i <= endno; i++)
+        {
+            changePosition((cht + stno), (cht + i));
+            charPermu(cht, stno + 1, endno);
+            changePosition((cht + stno), (cht + i));
+        }
+    }
 }
 
-void printCardInfo(char* cardOwner, void** card) {
-	printf("<%s> - %s\n", cardOwner, (char*)*card);	// (char*)card[0]
-	printf("|통솔|무력|지력|정치|매력|\n");
-	for (int i = 0; i < 5; i++)
-		printf("| %d ", *((int*)*(card + 1) + i));	// *((int*)card[1] + i)
-	printf("|\n");
-}
+int main()
+{
+    char str[10];
+    printf("\n\n Pointer : Generate permutations of a given string :\n");
+    printf("--------------------------------------------------------\n");
+    printf(" The permutations of the string are : \n");
+    scanf("%s", str);
+    int n = strlen(str);
 
-void swapCard(void** c1, void** c2) {
-	void* temp = *c1;
-	*c1 = *c2;
-	*c2 = temp;
-}
-
-void cardBattle(void** myCard, void** comCard) {
-	int sumA = 0, sumB = 0;
-	for (int i = 0; i < 5; i++) {
-		sumA += *((int*)*(myCard + 1) + i);
-		sumB += *((int*)*(comCard + 1) + i);
-	}
-	printf("\n************* 카드 배틀 *************\n");
-	if (sumA > sumB)
-		printf("당신 카드 점수(%d) vs 컴퓨터 카드 점수(%d)로 승리했습니다.\n", sumA, sumB);
-	else
-		printf("당신 카드 점수(%d) vs 컴퓨터 카드 점수(%d)로 패배했습니다.\n", sumA, sumB);
+    charPermu(str, 0, n - 1);
+    printf("\n\n");
+    return 0;
 }
