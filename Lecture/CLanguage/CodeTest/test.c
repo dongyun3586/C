a1d215785a42623b3536934094cb0224739def9b
leedong <goodluck3586@gmail.com>
2021-09-10 16:30:40 +0900

    문자열 예제 추가

--- Lecture/CLanguage/CodeTest/test.c
+++ Lecture/CLanguage/CodeTest/test.c
@@ -1,19 +1,19 @@
+#define _CRT_NONSTDC_NO_WARNINGS    // 비표준 함수를 사용했을 때 에러 방지
+#define _CRT_SECURE_NO_WARNINGS     // scanf 보안 경고로 인한 컴파일 에러 방지
 #include <stdio.h>
-#include<string.h>
+#include <conio.h>
 
-#define STRLEN 14
-
-int main(void)
+int main()
 {
-    char* src = "Take the test.";
-    char dst[STRLEN + 1];   // +1 to accomodate for the null terminator
+    int num1, num2;
 
-    strcpy(dst, src);
-    dst[0] = 'M';
-    printf("src = %s\ndst = %s\n", src, dst);
+    printf("숫자 두 개를 입력하세요: ");
+    scanf("%d %d", &num1, &num2);
 
-    src = malloc(sizeof(char) * 15);
-    strcpy(src, "Hello world");
-    printf("%s\n", src);
-    free(src);
+    printf("두 숫자의 합은 %d입니다.\n", num1 + num2);
+
+    printf("아무 키나 입력하면 콘솔 창을 닫습니다.\n");
+    getch();    // 키를 입력할 때까지 콘솔 창이 계속 켜져 있도록 만듦
+
+    return 0;
 }
