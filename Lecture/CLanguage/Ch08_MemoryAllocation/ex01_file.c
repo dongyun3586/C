db5c351a35801b71ef11ac8bb0fd1fe5eefa0f0b
leedong <goodluck3586@gmail.com>
2021-11-09 16:28:46 +0900

    gg

--- Lecture/CLanguage/Ch08_MemoryAllocation/ex01_file.c
+++ Lecture/CLanguage/Ch08_MemoryAllocation/ex01_file.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main() {
-	char* fileName = "test.txt";
+	char* fileName = "C:/Users/Administrator/Desktop/test.txt";
 	FILE* fp = NULL;
 	fp = fopen(fileName, "w");
 
@@ -13,7 +13,9 @@ int main() {
 		printf("The %s file was opened.\n", fileName);
 	}
 
+	// 파일을 이용한 어떤 입출력 작업...
+
 	fclose(fp);
 	printf("The %s file was closed.\n", fileName);
-	
+
 }
--- Lecture/CLanguage/CodeTest/test.c
+++ Lecture/CLanguage/CodeTest/test.c
@@ -1,62 +1,18 @@
 #include<stdio.h>
 
-typedef struct {
-	char name[10];
-	int info[5];
-}card;
-
-void printCardInfo(char* cardOwner, card* card);
-void swapCard(card** c1, card** c2);
-void cardBattle(void** myCard, void** comCard);
-
 int main() {
-	card A = { "유비", { 75, 73, 74, 78, 99 } };
-	card B = { "조조", { 96, 72, 91, 94, 96 } };
+	FILE* fp = fopen("test.txt", "w");
 
-	card* myCard = &A;
-	card* comCard = &B;
+	if (!fp) {
+		printf("Fail to open the file as the write mode.\n");
+		return 0;
+	}
 
-	printCardInfo("플레이어 카드", myCard);
-	printCardInfo("컴퓨터 카드", comCard);
+	fputc('a', fp);
+	fputc('B', fp);
+	fputc('1', fp);
 
-	cardBattle(myCard, comCard);
-
-	printf("\n****** Card trading ******\n\n");
-	swapCard(&myCard, &comCard);
-
-	printCardInfo("플레이어 카드", myCard);
-	printCardInfo("컴퓨터 카드", comCard);
-
-	cardBattle(myCard, comCard);
+	fclose(fp);
 
 	return 0;
-}
-
-void printCardInfo(char* cardOwner, card* card) {
-	printf("%s - %s\n", cardOwner, card->name);	// (char*)card[0]
-	printf("|통솔|무력|지력|정치|매력|\n");
-	for (int i = 0; i < 5; i++)
-		printf("| %d ", card->info[i]);	// *((int*)card[1] + i)
-	printf("|\n\n");
-}
-
-void swapCard(card** c1, card** c2) {
-	card* temp = *c1;
-	*c1 = *c2;
-	*c2 = temp;
-}
-
-void cardBattle(card* myCard, card* comCard) {
-	int sumA = 0, sumB = 0;
-	for (int i = 0; i < 5; i++) {
-		sumA += myCard->info[i];
-		sumB += comCard->info[i];
-	}
-	printf("************* 카드 배틀 *************\n");
-	printf("당신의 카드 점수 vs 컴퓨터 카드 점수\n");
-	printf("             %d vs %d\n", sumA, sumB);
-	if (sumA > sumB)
-		printf("======> 당신이 이겼습니다.\n");
-	else
-		printf("======> 컴퓨터가 이겼습니다.\n");
 }
