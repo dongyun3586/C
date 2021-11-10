0b8d6a5a46c2705f7e6573506f239ad67b1c04f2
leedong <goodluck3586@gmail.com>
2021-11-10 13:07:09 +0900

    2학기 기본학습 프로그램 문제 추가

---
+++ Lecture/CLanguage/2학기 기초보충/ex4_insertElement.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void print1DArray(int arr[], int n);
+void insertElement(int arr[], int n);
+
+int main()
+{
+    int numArr[100] = { 1,2,3,4,5,6,7,8,9,10 };
+    int n = 10;
+
+    // 배열 출력 
+    print1DArray(numArr, n);
+
+    for (int i = 0; i < 2; i++) {
+        insertElement(numArr, n++);      // 배열에 새로운 요소 삽입
+        print1DArray(numArr, n);         // 배열 출력 
+    }
+
+    return 0;
+}
+
+void print1DArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void insertElement(int arr[], int n)
+{
+    int p, value;
+    printf("위치와 새로운 값 입력: ");
+    scanf("%d %d", &p, &value);
+
+    for (int i = n - 1; i >= p - 1; i--)
+        arr[i + 1] = arr[i];
+    arr[p - 1] = value;
+}
--- Lecture/CLanguage/CodeTest/test.c
+++ Lecture/CLanguage/CodeTest/test.c
@@ -1,18 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+#define SIZE 10
+int bsearch(int arr[], int length, int findNum);
 
-int main() {
-	FILE* fp = fopen("test.txt", "w");
+int main()
+{
+    int findNum, index;
+    int numArr[] = { 3, 6, 7, 8, 11, 13, 16, 17, 18, 20 };
 
-	if (!fp) {
-		printf("Fail to open the file as the write mode.\n");
-		return 0;
-	}
+    printf("찾을 값: ");
+    scanf("%d", &findNum);
+    if (           가            )
+        printf("%d은 배열의 %d 번째 위치\n", findNum, index + 1);
+    else
+        printf("%d은 배열에 없습니다.\n", findNum);
+    return 0;
+}
 
-	fputc('a', fp);
-	fputc('B', fp);
-	fputc('1', fp);
-
-	fclose(fp);
-
-	return 0;
+int bsearch(int arr[], int length, int findNum) {
+    int low = 0, high = length - 1, mid, count = 0;
+    while (low <= high) {
+        printf("[%d %d]\n", low, high);
+        mid = (low + high) / 2;
+        if (findNum == arr[mid]) {
+            return mid;
+        }
+        else if (findNum > arr[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return 0;
 }
