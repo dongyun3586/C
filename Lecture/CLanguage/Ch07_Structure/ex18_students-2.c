a440e0b960dc37ce8235f77cee9a3c59f4510a17
leedong <goodluck3586@gmail.com>
2021-11-15 08:47:48 +0900

    gg

--- Lecture/CLanguage/Ch07_Structure/ex18_students-2.c
+++ Lecture/CLanguage/Ch07_Structure/ex18_students-2.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define MAXNAME 64      // 이름의 최대 길이
-int heapUsage = 0;      // 힙 메모리 사용량 저장 변수
+int usedMemory = 0;      // 힙 메모리 사용량 저장 변수
 
 typedef struct {
     int age;
@@ -11,40 +11,40 @@ typedef struct {
     char* name;         // char name[MAXNAME];
 } student;
 
-int setStudent(student* _dst) {
-    _dst->age = rand() % 10;
-    _dst->score = rand() % 100;
+int setStudent(student* s) {
+    s->age = rand() % 10;
+    s->score = rand() % 100;
 
     int nameLen = rand() % MAXNAME;
-    _dst->name = (char*)calloc(nameLen, sizeof(char));  // 이름을 저장할 메모리 할당
-    heapUsage += sizeof(char) * nameLen;                // 할당된 메모리 크기 누계
+    s->name = (char*)calloc(nameLen, sizeof(char));  // 이름을 저장할 메모리 할당
+    usedMemory += sizeof(char) * nameLen;                // 할당된 메모리 크기 누계
     for (int i = 0; i < nameLen; i++)
-        _dst->name[i] = 'a' + rand() % 26;
+        s->name[i] = 'a' + rand() % 26;
 }
 
 int main() {
-    int numStudent = 0;
+    int n = 0;
     printf("How many students? ");
-    scanf("%d", &numStudent);
+    scanf("%d", &n);
 
-    // numStudent 개수 만큼의 student 사이즈 메모리 할당
-    student* students = (student*)malloc(sizeof(student) * numStudent);
-    memset(students, 0, sizeof(student) * numStudent);  // 0 으로 초기화
-    heapUsage += sizeof(student) * numStudent;          // 할당된 메모리 크기 누계
+    // n 개수 만큼의 student 사이즈 메모리 할당
+    student* students = (student*)malloc(sizeof(student) * n);
+    memset(students, 0, sizeof(student) * n);  // 0 으로 초기화
+    usedMemory += sizeof(student) * n;          // 할당된 메모리 크기 누계
 
-    // numStudent 개수 만큼 랜덤한 학생정보 저장
-    for (int i = 0; i < numStudent; i++)
+    // n 개수 만큼 랜덤한 학생정보 저장
+    for (int i = 0; i < n; i++)
         setStudent(&students[i]);
 
     // 평균 계산 및 출력
     float avg = 0;
-    for (int i = 0; i < numStudent; i++)
+    for (int i = 0; i < n; i++)
         avg += students[i].score;
-    avg /= numStudent;
+    avg /= n;
     printf("Average score = %.2f\n", avg);
 
     // release name for each student
-    for (int i = 0; i < numStudent; i++)
+    for (int i = 0; i < n; i++)
         if (students->name) {
             free(students->name);
             students->name = NULL;
@@ -56,7 +56,7 @@ int main() {
         students = NULL;
     }
 
-    printf("heap memory usage = %d bytes\n", heapUsage);
+    printf("heap memory usage = %d bytes\n", usedMemory);
 
     return 0;
 }
--- Lecture/CLanguage/CodeTest/test.c
+++ Lecture/CLanguage/CodeTest/test.c
@@ -1,24 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define MAX_SIZE 100 
-// ① removeLeading( ) 함수 [8점]
 
+#define MAXNAME 16      // 이름의 최대 길이
+int usedMemory = 0;      // 힙 메모리 사용량 저장 변수
 
+typedef struct {
+    int age;
+    float score;
+    char* name;         // char name[MAXNAME];
+} student;
 
-// ② removeTrailing( ) 함수 [8점]
+int setStudent(student* s) {
+    s->age = rand() % 10+1;
+    s->score = rand() % 100;
 
+    int nameLen = rand() % MAXNAME + 1;                  
+    s->name = (char*)calloc(nameLen, sizeof(char));  // 이름을 저장할 메모리 할당
+    usedMemory += sizeof(char) * nameLen;                // 할당된 메모리 크기 누계
+    for (int i = 0; i < nameLen - 1; i++)
+        s->name[i] = 'a' + rand() % 26;
+    s->name[nameLen-1] = '\0';
+    printf("%d %.2f %s\n", s->age, s->score, s->name);
+}
 
+int main() {
+    int n = 0;
+    printf("How many students? ");
+    scanf("%d", &n);
 
-int main()
-{
-    char str[MAX_SIZE];
-    printf("문자열 입력: ");
-    gets(str);
+    // n 개수 만큼의 student 사이즈 메모리 할당
+    student* students = (student*)malloc(sizeof(student) * n);
+    memset(students, 0, sizeof(student) * n);  // 0 으로 초기화
+    usedMemory += sizeof(student) * n;          // 할당된 메모리 크기 누계
 
-    printf("입력된 문자열: '%s'\n", str);
-    removeLeading(str);
-    printf("앞쪽 공백 제거: '%s'\n", str);
-    removeTrailing(str);
-    printf("뒷쪽 공백 제거: '%s'\n", str);
+    // n 개수 만큼 랜덤한 학생정보 저장
+    for (int i = 0; i < n; i++)
+        setStudent(&students[i]);
+
+    // 평균 계산 및 출력
+    float avg = 0;
+    for (int i = 0; i < n; i++)
+        avg += students[i].score;
+    avg /= n;
+    printf("Average score = %.2f\n", avg);
+
+    // release name for each student
+    for (int i = 0; i < n; i++)
+        if (students->name) {
+            free(students->name);
+            students->name = NULL;
+        }
+
+    // release students
+    if (students) {
+        free(students);
+        students = NULL;
+    }
+
+    printf("heap memory usage = %d bytes\n", usedMemory);
+
     return 0;
 }
