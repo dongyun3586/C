bb187a7b80f91b41a480b77dcf6e33aa280a8146
leedong <goodluck3586@gmail.com>
2022-03-04 07:53:51 +0900

    gg
    gg

--- Lecture/CLanguage/CodeTest/test.c
+++ Lecture/CLanguage/CodeTest/test.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
-#include <ctype.h>
 
-void func();
-
-int count = 0;
+void printArray(int* arr, int n);
+void swapArray(int** arr1, int** arr2);
 
 int main()
 {
-    char c;
-    while ((c = getchar()) != EOF) {
-        if (isalpha(c)) {
-            count++;
-            printf("main: %c\n", c);
-            func();
-        }
-    }
+    int arr1[] = { 1,2,3,4,5 };
+    int arr2[] = { 10,20,30,40,50 };
 
-    printf("count: %d\n", count);
+    int* p1 = arr1;
+    int* p2 = arr2;
+
+    printArray(p1, 5);
+    printArray(p2, 5);
+
+    swapArray(&p1, &p2);
+
+    printArray(p1, 5);
+    printArray(p2, 5);
+
     return 0;
 }
 
-void func() {
-    char c;
-    c = getchar();
+void printArray(int* arr, int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-    if (isalpha(c)) {
-        count++;
-        printf("func: %c\n", c);
-    }
+void swapArray(int** arr1, int** arr2) {
+    int* temp = *arr1;
+    *arr1 = *arr2;
+    *arr2 = temp;
 }
